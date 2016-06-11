ea6408ec8ee7af292aa5b611ed2580e8c49371e3
kaien <contact@kaienx.com>
2016-06-11 14:22:33 +0800

    working fade

---
+++ web/js/seth.js
@@ -0,0 +1,11 @@
+﻿
+var swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    autoplay: 5500,
+    loop: true,
+    autoplayDisableOnInteraction: false,
+    effect: 'fade',
+    speed: 2500
+});
+new WOW().init();
+$('body').removeClass('hidden');

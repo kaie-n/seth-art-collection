a59c13b915fd47ebc1098fa31e1ab0bdf4323392
kaien <contact@kaienx.com>
2016-06-18 15:51:38 +0800

    test

--- web/js/seth.js
+++ web/js/seth.js
@@ -1,11 +1,12 @@
 ﻿
 var swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
-    autoplay: 5500,
+    autoplay: 6500,
     loop: true,
     autoplayDisableOnInteraction: false,
     effect: 'fade',
     speed: 2500
 });
 new WOW().init();
-$('body').removeClass('hidden');
+
+$('.overlay').fadeOut();
